6b0d2f44512ef46c22098de8e2ff29ae0f83f280
Anuprita <anupritamhapankar22@gmail.com>
2023-12-03 23:40:23 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
+
 import express from "express";
+
+import dotenv  from "dotenv"
+dotenv.config()
 
 const app = express();
 
-const port = 9000;
+// app.use("/", (req, res) => {
+//   res.json({ message: "Hello Vercel" });
+// });
 
-app.use("/", (req, res) => {
-  res.json({ message: "Hello Vercel" });
+app.get("/", (req, res) => {
+  res.json({ message: "Home Page" });
 });
 
-app.listen(9000, () => {
-  console.log(`Server is running on Port ${port}`);
+app.get("/login", (req, res) => {
+  res.json({ message: "Login Page" });
+});
+
+app.get("/register", (req, res) => {
+  res.json({ message: "Register Page" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on Port ${process.env.PORT}`);
 });
